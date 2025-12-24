2e4893619096a6568bd4b0ea54998cb0f7e0ca4b
Luciano de Lusarreta <luciano@example.com>
2025-12-23 21:56:37 -0800

    Fix build: add lib/constants

---
+++ lib/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_NAME = "LilMovements";
+export const BRAND_NAME = "LilMovements";
+export const DEFAULT_OG_IMAGE = "";
+export const SOCIALS = { instagram: "", youtube: "", tiktok: "" };
